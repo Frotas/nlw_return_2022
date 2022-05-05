64e38f3e5f59a71d4a461db1a692eb65a67a296b
frotas <guilherme.frotas19@gmail.com>
2022-05-05 20:23:01 -0300

    :sparkles: feat: Menu mobile adicionado.

--- public/javascript/main.js
+++ public/javascript/main.js
@@ -1,5 +1,15 @@
-let navgationScroll = document.querySelector('.header');
+let container = document.querySelector(".container");
+let navgationScroll = document.querySelector(".header");
+let navgationMenu = document.querySelector(".menu");
 
 function onScroll() {
-  navgationScroll.classList.toggle('scroll', window.scrollY > 0);
+  navgationScroll.classList.toggle("scroll", window.scrollY > 0);
+}
+
+function menuOpen() {
+  container.classList.add("menu--expanded");
+}
+
+function menuClose() {
+  container.classList.remove("menu--expanded");
 }
